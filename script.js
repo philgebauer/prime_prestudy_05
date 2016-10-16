61a2fc1d09b43b3332a4f049b1465e26ebf30556
Phil G <philg@Phils-MacBook-Pro.local>
2016-10-16 12:12:46 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+var avengersMovie = {
+    name : "Avengers",                  //Mostly small changes were made -
+    runtimeInMinutes : 143,             // semi-colons -> commas
+    releaseYear : 2012,                 // quotation marks around strings
+    director : "Joss Whedon"            // capital 'D' in director -> lowercase
+}
+
+console.log(avengersMovie);
+
+  $(document).ready(function(){
+      $("#click").click(function(){
+          $('h2').toggle();
+
+
+      });
+});
